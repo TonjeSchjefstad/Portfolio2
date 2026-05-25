785a5403bd99b2c16339d3d0886404f7aba39993
TonjeSchjefstad <tonsch03841@stud.noroff.no>
2026-05-25 14:59:39 +0200

    Add folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-navy"></h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">Hello, World!</h1>
     </div>
   )
 }
